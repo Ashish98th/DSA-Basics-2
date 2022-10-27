e4b78142e9b9cb50cb445856789462b6281a6bc3
Ashish98th <ashishnathofficia98@gmail.com>
2022-10-27 22:23:41 +0530

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int age=18;
+    if(age==8){
+        System.out.println("Yes boy You can ride");
+    }
+   else{
+       System.out.println("No");
+    }
+
+
+    }
+}
---
+++ src/else_if_switch_case.java
@@ -0,0 +1,19 @@
+public class else_if_switch_case {
+    public static void main(String[] args) {
+        int i=7;
+
+        switch(i){
+
+               case 0:
+                   System.out.println("zero");
+            case 1:
+                System.out.println("one");
+            case 2:
+                System.out.println("two");
+            case 7:
+                System.out.println("seven");
+            case 9:
+                System.out.println("Nine");
+       }
+    }
+}
